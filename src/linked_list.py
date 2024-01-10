1750f0957991b40b897ff373bd6c3c257209c38e
aleksandr <89191215757@mail.ru>
2024-01-10 10:33:45 +0500

    Do homework-5

--- src/linked_list.py
+++ src/linked_list.py
@@ -1,18 +1,54 @@
 class Node:
     """Класс для узла односвязного списка"""
-    pass
+
+    def __init__(self, data):
+        """
+        Конструктор класса Node
+
+        :param data: данные, которые будут храниться в узле
+        """
+        self.data = data
+        self.next_node = None
 
 
 class LinkedList:
     """Класс для односвязного списка"""
+    linked_list_size = 0
+    linked_list_data = []
+
+    def __init__(self):
+        self.head = None
+        self.tail = None
 
     def insert_beginning(self, data: dict) -> None:
         """Принимает данные (словарь) и добавляет узел с этими данными в начало связанного списка"""
-        pass
+
+        node = Node(data)
+        if self.__class__.linked_list_size == 0:
+            self.head = node
+            self.tail = node
+            self.__class__.linked_list_data.append(self.head.data)
+        else:
+            curr = self.head
+            self.head = node
+            self.head.next_node = curr
+            self.__class__.linked_list_data.insert(0, self.head.data)
+
+        self.__class__.linked_list_size += 1
 
     def insert_at_end(self, data: dict) -> None:
         """Принимает данные (словарь) и добавляет узел с этими данными в конец связанного списка"""
-        pass
+
+        node = Node(data)
+        if self.__class__.linked_list_size == 0:
+            self.head = node
+            self.tail = node
+        else:
+            self.tail.next_node = node
+            self.tail = node
+
+        self.__class__.linked_list_size += 1
+        self.__class__.linked_list_data.append(data)
 
     def __str__(self) -> str:
         """Вывод данных односвязного списка в строковом представлении"""
@@ -26,4 +62,4 @@ class LinkedList:
             node = node.next_node
 
         ll_string += ' None'
-        return ll_string
+        return ll_string.lstrip()
--- tests/test_linked_list.py
+++ tests/test_linked_list.py
@@ -1,1 +1,61 @@
 """Здесь надо написать тесты с использованием unittest для модуля linked_list."""
+
+from src.linked_list import Node, LinkedList
+
+node = Node(None)
+ll = LinkedList()
+
+
+## ____________________________________________________tests for Node
+def test__node_init():
+    assert isinstance(node, Node)
+    assert node.data is None
+    assert node.next_node is None
+
+
+## ______________________________________________tests for LinkedList
+def test__init():
+    assert ll.head is None
+    assert ll.tail is None
+    assert ll.linked_list_size == 0
+    assert ll.linked_list_data == []
+
+
+def test__str1():
+    assert str(ll) == 'None'
+
+
+def test__insert_beginning():
+    ll.insert_beginning('test1')
+    assert ll.head.data == 'test1'
+    assert ll.head.next_node is None
+    assert ll.tail.data == 'test1'
+    assert ll.tail.next_node is None
+    assert ll.linked_list_data == ['test1']
+    assert ll.linked_list_size == 1
+
+    ll.insert_beginning({'key': 1})
+    assert ll.head.data == {'key': 1}
+    assert ll.head.next_node.data == 'test1'
+    assert ll.tail.data == 'test1'
+    assert ll.tail.next_node is None
+    assert ll.linked_list_data == [{'key': 1}, 'test1']
+    assert ll.linked_list_size == 2
+
+def test__str2():
+    assert str(ll) == "{'key': 1} -> test1 -> None"
+
+
+def test__insert_at_end():
+    ll.insert_at_end('test2')
+    assert ll.head.data == {'key': 1}
+    assert ll.head.next_node.data == 'test1'
+    assert ll.head.next_node.next_node.data == 'test2'
+    assert ll.tail.data == 'test2'
+    assert ll.tail.next_node is None
+    assert ll.linked_list_data == [{'key': 1}, 'test1', 'test2']
+    assert ll.linked_list_size == 3
+
+
+def test__str3():
+    assert str(ll) == "{'key': 1} -> test1 -> test2 -> None"
